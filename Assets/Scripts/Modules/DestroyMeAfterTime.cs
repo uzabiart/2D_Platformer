4e9075c138579295f247389fffd7ecca8cc50fa6
PKwiatkowski <pkwiatkowski@neatware.pl>
2021-01-31 19:39:31 +0100

    graf

---
+++ Assets/Scripts/Modules/DestroyMeAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMeAfterTime : MonoBehaviour
+{
+    public float time;
+
+    void Start()
+    {
+        Invoke(nameof(DestroyMe), time);
+    }
+
+    void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+}
