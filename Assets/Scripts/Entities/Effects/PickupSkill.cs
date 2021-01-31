aa079b4848bc837f8653b19fffbe115a3815f63f
PKwiatkowski <pkwiatkowski@neatware.pl>
2021-01-31 15:43:50 +0100

    pickups

---
+++ Assets/Scripts/Entities/Effects/PickupSkill.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSkill : Entity
+{
+    public SpriteRenderer skillIcon;
+    public Transform skillPrefabParent;
+
+    public void UpdateMe(SkillData skill)
+    {
+        skillIcon.sprite = skill.skillIcon;
+        Instantiate(skill.skillPrefab, skillPrefabParent);
+    }
+}
---
+++ Assets/Scripts/_Data/SkillData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "UMI/New Skill", fileName = "Skill")]
+public class SkillData : MonoBehaviour
+{
+    public Sprite skillIcon;
+    public GameObject skillPrefab;
+}
